578722eccb8024c753649c3d8b3d13b43fe3d3dd
Mikko Järvi <lego@lego.(none)>
2015-01-02 03:57:18 +0200

    ensimmäisiä testejä

---
+++ src/org/lejos/example/Lajittelurobo.java
@@ -0,0 +1,15 @@
+package lajittelurobo;
+
+import lejos.nxt.*;
+
+/**
+ * Example leJOS Project with an ant build file
+ *
+ */
+public class Lajittelurobo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World ja testausta");
+		Button.waitForPress();
+	}
+}
