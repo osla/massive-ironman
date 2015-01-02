9b5c0646eff5720fdf573a9ed6d843f0070c33c3
Mikko Järvi <mikko.jarvi@gmail.com>
2015-01-02 04:39:49 +0200

    valosensorin testausta

--- src/org/lejos/example/Lajittelurobo.java
+++ src/org/lejos/example/Lajittelurobo.java
@@ -8,8 +8,36 @@ import lejos.nxt.*;
  */
 public class Lajittelurobo {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World ja testausta");
-		Button.waitForPress();
+	public static void main(String[] args) throws Exception {
+
+		Button.waitForPress();		
+		LightSensor ls = new LightSensor(SensorPort.S2);
+		LCD.drawString("Valoa %: ", 0, 0);
+		LCD.drawString("Paina VASEN", 0, 2);
+		LCD.drawString("aloittaaksesi", 0, 3);
+		LCD.drawString("lajittelun.", 0, 4);
+		while (! Button.LEFT.isPressed()){
+			LCD.drawInt(ls.readValue(), 3, 9, 0);
+		}
+		LCD.drawString("Paina ESCAPE", 0, 6);
+		LCD.drawString("lopettaaksesi", 0, 7);
+		int i = 0;
+		while (! Button.ESCAPE.isPressed()){
+			while (i < 5){
+				tiputaYksi();
+				i++;
+			};
+		}
+		Button.waitForPress(10000);	
+	}
+
+	public static boolean tiputaYksi() throws Exception {
+		Motor.C.setSpeed(100);
+		Motor.C.backward();
+		Thread.sleep(345);//pienen legon pituus = 345ms , pitkän pituus xxx
+		Motor.C.stop();
+		Thread.sleep(3000);
+		return true;
 	}
 }
+
